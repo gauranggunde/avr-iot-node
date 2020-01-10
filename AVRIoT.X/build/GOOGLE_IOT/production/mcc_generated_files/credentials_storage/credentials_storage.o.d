ffd4945ee1dd90db5f873d7e321570517af00771
Microchip Technology <microchip@microchip.com>
2020-01-10 17:36:51 +0000

    Merging into the (develop) branch

---
+++ AVRIoT.X/build/GOOGLE_IOT/production/mcc_generated_files/credentials_storage/credentials_storage.o.d
@@ -0,0 +1,54 @@
+build/GOOGLE_IOT/production/mcc_generated_files/credentials_storage/credentials_storage.o.d \
+ build/GOOGLE_IOT/production/mcc_generated_files/credentials_storage/credentials_storage.o: \
+ mcc_generated_files/credentials_storage/credentials_storage.c \
+ /opt/microchip/xc8/v2.10/avr/lib/gcc/avr/5.4.0/include/stdint.h \
+ /opt/microchip/xc8/v2.10/avr/avr/include/stdint.h \
+ /opt/microchip/xc8/v2.10/avr/avr/include/string.h \
+ /opt/microchip/xc8/v2.10/avr/lib/gcc/avr/5.4.0/include/stddef.h \
+ /opt/microchip/xc8/v2.10/avr/avr/include/avr/eeprom.h \
+ /opt/microchip/xc8/v2.10/avr/avr/include/avr/io.h \
+ /opt/microchip/xc8/v2.10/avr/avr/include/avr/sfr_defs.h \
+ /opt/microchip/xc8/v2.10/avr/avr/include/inttypes.h \
+ /opt/microchip/mplabx/v5.30/packs/Microchip/ATmega_DFP/2.0.12/include/avr/iom4808.h \
+ /opt/microchip/xc8/v2.10/avr/avr/include/avr/portpins.h \
+ /opt/microchip/xc8/v2.10/avr/avr/include/avr/common.h \
+ /opt/microchip/xc8/v2.10/avr/avr/include/avr/version.h \
+ /opt/microchip/xc8/v2.10/avr/avr/include/avr/xmega.h \
+ /opt/microchip/xc8/v2.10/avr/avr/include/avr/fuse.h \
+ /opt/microchip/xc8/v2.10/avr/avr/include/avr/lock.h \
+ /opt/microchip/xc8/v2.10/avr/avr/include/stdlib.h \
+ mcc_generated_files/credentials_storage/credentials_storage.h
+
+/opt/microchip/xc8/v2.10/avr/lib/gcc/avr/5.4.0/include/stdint.h:
+
+/opt/microchip/xc8/v2.10/avr/avr/include/stdint.h:
+
+/opt/microchip/xc8/v2.10/avr/avr/include/string.h:
+
+/opt/microchip/xc8/v2.10/avr/lib/gcc/avr/5.4.0/include/stddef.h:
+
+/opt/microchip/xc8/v2.10/avr/avr/include/avr/eeprom.h:
+
+/opt/microchip/xc8/v2.10/avr/avr/include/avr/io.h:
+
+/opt/microchip/xc8/v2.10/avr/avr/include/avr/sfr_defs.h:
+
+/opt/microchip/xc8/v2.10/avr/avr/include/inttypes.h:
+
+/opt/microchip/mplabx/v5.30/packs/Microchip/ATmega_DFP/2.0.12/include/avr/iom4808.h:
+
+/opt/microchip/xc8/v2.10/avr/avr/include/avr/portpins.h:
+
+/opt/microchip/xc8/v2.10/avr/avr/include/avr/common.h:
+
+/opt/microchip/xc8/v2.10/avr/avr/include/avr/version.h:
+
+/opt/microchip/xc8/v2.10/avr/avr/include/avr/xmega.h:
+
+/opt/microchip/xc8/v2.10/avr/avr/include/avr/fuse.h:
+
+/opt/microchip/xc8/v2.10/avr/avr/include/avr/lock.h:
+
+/opt/microchip/xc8/v2.10/avr/avr/include/stdlib.h:
+
+mcc_generated_files/credentials_storage/credentials_storage.h:
---
+++ AVRIoT.X/build/GOOGLE_IOT/production/mcc_generated_files/sensors_handling.o.d
@@ -0,0 +1,81 @@
+build/GOOGLE_IOT/production/mcc_generated_files/sensors_handling.o.d \
+ build/GOOGLE_IOT/production/mcc_generated_files/sensors_handling.o: \
+ mcc_generated_files/sensors_handling.c \
+ /opt/microchip/xc8/v2.10/avr/lib/gcc/avr/5.4.0/include/stdint.h \
+ /opt/microchip/xc8/v2.10/avr/avr/include/stdint.h \
+ mcc_generated_files/sensors_handling.h \
+ mcc_generated_files/include/adc0.h \
+ mcc_generated_files/include/../utils/compiler.h \
+ /opt/microchip/xc8/v2.10/avr/avr/include/avr/io.h \
+ /opt/microchip/xc8/v2.10/avr/avr/include/avr/sfr_defs.h \
+ /opt/microchip/xc8/v2.10/avr/avr/include/inttypes.h \
+ /opt/microchip/mplabx/v5.30/packs/Microchip/ATmega_DFP/2.0.12/include/avr/iom4808.h \
+ /opt/microchip/xc8/v2.10/avr/avr/include/avr/portpins.h \
+ /opt/microchip/xc8/v2.10/avr/avr/include/avr/common.h \
+ /opt/microchip/xc8/v2.10/avr/avr/include/avr/version.h \
+ /opt/microchip/xc8/v2.10/avr/avr/include/avr/xmega.h \
+ /opt/microchip/xc8/v2.10/avr/avr/include/avr/fuse.h \
+ /opt/microchip/xc8/v2.10/avr/avr/include/avr/lock.h \
+ /opt/microchip/xc8/v2.10/avr/avr/include/avr/builtins.h \
+ /opt/microchip/xc8/v2.10/avr/lib/gcc/avr/5.4.0/include/stdbool.h \
+ /opt/microchip/xc8/v2.10/avr/lib/gcc/avr/5.4.0/include/stddef.h \
+ /opt/microchip/xc8/v2.10/avr/avr/include/stdlib.h \
+ mcc_generated_files/include/../utils/interrupt_avr8.h \
+ /opt/microchip/xc8/v2.10/avr/avr/include/avr/interrupt.h \
+ mcc_generated_files/drivers/i2c_simple_master.h \
+ /opt/microchip/xc8/v2.10/avr/avr/include/stdio.h \
+ /opt/microchip/xc8/v2.10/avr/lib/gcc/avr/5.4.0/include/stdarg.h \
+ mcc_generated_files/drivers/../include/twi0_master.h \
+ mcc_generated_files/drivers/../include/../utils/compiler.h
+
+/opt/microchip/xc8/v2.10/avr/lib/gcc/avr/5.4.0/include/stdint.h:
+
+/opt/microchip/xc8/v2.10/avr/avr/include/stdint.h:
+
+mcc_generated_files/sensors_handling.h:
+
+mcc_generated_files/include/adc0.h:
+
+mcc_generated_files/include/../utils/compiler.h:
+
+/opt/microchip/xc8/v2.10/avr/avr/include/avr/io.h:
+
+/opt/microchip/xc8/v2.10/avr/avr/include/avr/sfr_defs.h:
+
+/opt/microchip/xc8/v2.10/avr/avr/include/inttypes.h:
+
+/opt/microchip/mplabx/v5.30/packs/Microchip/ATmega_DFP/2.0.12/include/avr/iom4808.h:
+
+/opt/microchip/xc8/v2.10/avr/avr/include/avr/portpins.h:
+
+/opt/microchip/xc8/v2.10/avr/avr/include/avr/common.h:
+
+/opt/microchip/xc8/v2.10/avr/avr/include/avr/version.h:
+
+/opt/microchip/xc8/v2.10/avr/avr/include/avr/xmega.h:
+
+/opt/microchip/xc8/v2.10/avr/avr/include/avr/fuse.h:
+
+/opt/microchip/xc8/v2.10/avr/avr/include/avr/lock.h:
+
+/opt/microchip/xc8/v2.10/avr/avr/include/avr/builtins.h:
+
+/opt/microchip/xc8/v2.10/avr/lib/gcc/avr/5.4.0/include/stdbool.h:
+
+/opt/microchip/xc8/v2.10/avr/lib/gcc/avr/5.4.0/include/stddef.h:
+
+/opt/microchip/xc8/v2.10/avr/avr/include/stdlib.h:
+
+mcc_generated_files/include/../utils/interrupt_avr8.h:
+
+/opt/microchip/xc8/v2.10/avr/avr/include/avr/interrupt.h:
+
+mcc_generated_files/drivers/i2c_simple_master.h:
+
+/opt/microchip/xc8/v2.10/avr/avr/include/stdio.h:
+
+/opt/microchip/xc8/v2.10/avr/lib/gcc/avr/5.4.0/include/stdarg.h:
+
+mcc_generated_files/drivers/../include/twi0_master.h:
+
+mcc_generated_files/drivers/../include/../utils/compiler.h:
